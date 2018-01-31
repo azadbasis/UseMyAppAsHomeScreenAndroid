42d410b342faec0b7f71b5265c226be7466e5c05
Azharul Islam <azadbasis@gmail.com>
2018-01-31 18:10:40 +0600

    main final design

--- app/src/main/java/azhar/testlayoutvisibility/nanosoft/usemyappashomescreenandroid/FloatingMainActivity.java
+++ app/src/main/java/azhar/testlayoutvisibility/nanosoft/usemyappashomescreenandroid/FloatingMainActivity.java
@@ -1,16 +1,23 @@
 package azhar.testlayoutvisibility.nanosoft.usemyappashomescreenandroid;
 
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
 import android.icu.util.Calendar;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.ActivityCompat;
@@ -22,30 +29,34 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
-import android.view.Menu;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
-import com.github.clans.fab.FloatingActionButton;
-import com.github.clans.fab.FloatingActionMenu;
+import com.frosquivel.magicaltakephoto.MagicalTakePhoto;
 import com.google.firebase.auth.FirebaseAuth;
 import com.squareup.timessquare.CalendarPickerView;
 
 import org.joda.time.DateTime;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import azhar.testlayoutvisibility.nanosoft.usemyappashomescreenandroid.adapter.ScheduleAdapter;
 import azhar.testlayoutvisibility.nanosoft.usemyappashomescreenandroid.model.LoginResponse;
@@ -61,10 +72,10 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-import static android.Manifest.permission.GET_ACCOUNTS;
+import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.READ_PHONE_STATE;
-import static android.Manifest.permission_group.CAMERA;
 import static android.widget.LinearLayout.VERTICAL;
 import static com.squareup.timessquare.CalendarPickerView.SelectionMode.MULTIPLE;
 
@@ -76,6 +87,10 @@ public class FloatingMainActivity extends AppCompatActivity {
 
     Context con;
     private static final int PERMISSION_REQUEST_CODE = 200;
+    String[] permissionsRequired = new String[]{CAMERA,
+            ACCESS_FINE_LOCATION,
+            ACCESS_COARSE_LOCATION,READ_PHONE_STATE};
+
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth auth;
     private CalendarPickerView calendar_view;
@@ -92,7 +107,7 @@ public class FloatingMainActivity extends AppCompatActivity {
     private Dialog dialog;
     private ImageView imgCall,imgCam,imgMsg,imgEmail,imgChat;
 
-
+    MagicalTakePhoto magicalTakePhoto;
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,9 +115,9 @@ public class FloatingMainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         con = this;
-
+        magicalTakePhoto =  new MagicalTakePhoto(this,100);
         if(checkPermission()){
-            //queryLabels();
+
         }else if(!checkPermission()){
             requestPermission();
         }
@@ -135,7 +150,14 @@ public class FloatingMainActivity extends AppCompatActivity {
         imgCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(con,QrCodeScannerActivity.class));
+                photoDialogue();
+                if(checkPermission()){
+
+                }else {
+                    requestPermission();
+                }
+
+                //startActivity(new Intent(con,QrCodeScannerActivity.class));
             }
         });
 
@@ -239,21 +261,20 @@ public class FloatingMainActivity extends AppCompatActivity {
     }
 
     private boolean checkPermission() {
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
-        int result2 = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
-        int result3 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_PHONE_STATE);
-        int result4 = ContextCompat.checkSelfPermission(getApplicationContext(), GET_ACCOUNTS);
-        //int result5 = ContextCompat.checkSelfPermission(getApplicationContext(), CON);
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
+        int result2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_PHONE_STATE);
 
-        return result1 == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED &&
-                result3 == PackageManager.PERMISSION_GRANTED && result4 == PackageManager.PERMISSION_GRANTED;
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED && result2 == PackageManager.PERMISSION_GRANTED;
     }
+
     private void requestPermission() {
 
-        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, CAMERA,READ_PHONE_STATE,GET_ACCOUNTS}, PERMISSION_REQUEST_CODE);
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, CAMERA,READ_PHONE_STATE}, PERMISSION_REQUEST_CODE);
 
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
@@ -263,12 +284,8 @@ public class FloatingMainActivity extends AppCompatActivity {
                     boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                     boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
                     boolean readPhoneAccepted = grantResults[2] == PackageManager.PERMISSION_GRANTED;
-                    boolean getaccount = grantResults[3] == PackageManager.PERMISSION_GRANTED;
 
-
-                    //queryLabels();
-                    if (locationAccepted && cameraAccepted && readPhoneAccepted && getaccount) {
-
+                    if (locationAccepted && cameraAccepted && readPhoneAccepted) {
                         // Snackbar.make(view, "Permission Granted, Now you can access location data and camera.", Snackbar.LENGTH_LONG).show();
                     } else {
 
@@ -281,7 +298,7 @@ public class FloatingMainActivity extends AppCompatActivity {
                                             @Override
                                             public void onClick(DialogInterface dialog, int which) {
                                                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                                    requestPermissions(new String[]{ACCESS_FINE_LOCATION, CAMERA, GET_ACCOUNTS},
+                                                    requestPermissions(new String[]{ACCESS_FINE_LOCATION, CAMERA, READ_PHONE_STATE},
                                                             PERMISSION_REQUEST_CODE);
                                                 }
                                             }
@@ -307,7 +324,6 @@ public class FloatingMainActivity extends AppCompatActivity {
                 .create()
                 .show();
     }
-
     @RequiresApi(api = Build.VERSION_CODES.N)
     private void eventDialogue(final String date) {
 
@@ -535,10 +551,176 @@ public class FloatingMainActivity extends AppCompatActivity {
 
 
 
+   private void photoDialogue() {
+
+        final Dialog dialog = new Dialog(con);
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        dialog.setContentView(R.layout.cameradialog);
+
+        dialog.getWindow().setBackgroundDrawable(
+
+                new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        Button mCamerabtn = (Button) dialog.findViewById(R.id.cameradialogbtn);
+
+        Button mGallerybtn = (Button) dialog
+
+                .findViewById(R.id.gallerydialogbtn);
+
+        Button btnCancel = (Button) dialog.findViewById(R.id.canceldialogbtn);
+
+
+
+        dialog.getWindow().setLayout(LinearLayout.LayoutParams.FILL_PARENT,
+
+                LinearLayout.LayoutParams.FILL_PARENT);
+
+
+
+        mCamerabtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                magicalTakePhoto.takePhoto("sreda");
+                Intent intent1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent1, 0);
+                dialog.cancel();
+
+            }
+
+        });
+
+
+
+        mGallerybtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                startActivity(new Intent(con,QrCodeScannerActivity.class));
+                dialog.cancel();
+
+            }
+
+        });
+
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+                dialog.cancel(); // dismissing the popup
+
+            }
+
+        });
+
+
+
+        dialog.show();
+
+
+
+    }
+
+
+
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        magicalTakePhoto.resultPhoto(requestCode, resultCode, data);
+
+        // example to get photo
+        // imageView.setImageBitmap(magicalTakePhoto.getMyPhoto());
+        //saveToInternalStorage(magicalTakePhoto.getMyPhoto());
+        saveImageToExternalStorage(magicalTakePhoto.getMyPhoto());
+    }
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper cw = new ContextWrapper(getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath=new File(directory,"profile.jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
+    }
+
+    private void createDirectoryAndSaveFile(Bitmap imageToSave, String fileName) {
+
+        File direct = new File(Environment.getRootDirectory() + "/DirName");
+
+        if (!direct.exists()) {
+            File wallpaperDirectory = new File("/sdcard/EREDA/");
+            wallpaperDirectory.mkdirs();
+        }
+
+        File file = new File(new File("/sdcard/EREDA/"), fileName);
+        if (file.exists()) {
+            file.delete();
+        }
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            imageToSave.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveImageToExternalStorage(Bitmap finalBitmap) {
+        String root = Environment.getRootDirectory().toString();
+        File myDir = new File(root + "/sreda_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-" + n + ".jpg";
+        File file = new File(myDir, fname);
+        if (file.exists())
+            file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        // Tell the media scanner about the new file so that it is
+        // immediately available to the user.
+        MediaScannerConnection.scanFile(this, new String[]{file.toString()}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        Log.i("ExternalStorage", "Scanned " + path + ":");
+                        Log.i("ExternalStorage", "-> uri=" + uri);
+                    }
+                });
+
     }
 }
